ef252fd17e28e6d916afe0dded66289ad03b19a9
Sukumar <sukuappdev@gmail.com>
2025-01-19 22:55:21 +0530

    phone verify page worked

--- lib/views/auth/phone_verification_page.dart
+++ lib/views/auth/phone_verification_page.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:phone_otp_verification/phone_verification.dart';
+
+import '../../common/app_style.dart';
+import '../../constants/constants.dart';
 
 class PhoneVerificationPage extends StatelessWidget {
   const PhoneVerificationPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return PhoneVerification(
+      isFirstPage: false,
+      enableLogo: false,
+      themeColor: kPrimary,
+      backgroundColor: kLightWhite,
+      initialPageText: "Verify Phone Number",
+      initialPageTextStyle: appStyle(20, kPrimary, FontWeight.bold),
+      textColor: kDark,
+      onSend: (String value) {
+        print('Phone number: $value');
+      },
+      onVerification: (String value) {
+        print('OTP: $value');
+      },
+    );
   }
 }
